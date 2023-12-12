871bb46f31cb58d0fa0d556f44d505ff8383c066
NekkidBear <jmking2009@live.com>
2023-12-12 13:31:36 -0600

    initial commit

--- Install external daz files.py
+++ Install external daz files.py
@@ -16,23 +16,16 @@ for filename in os.listdir(new_folder_path):
 
         # Open the zip file
         with zipfile.ZipFile(filepath, 'r') as zip_ref:
-            # Loop through each file in the zip file
-            for file in zip_ref.namelist():
-                # Construct the path where the file would be if it was in the Daz library
-                daz_filepath = os.path.join(daz_library_path, file)
+            # Check if any file in the zip file does not exist in the Daz library
+            if not all(os.path.exists(os.path.join(daz_library_path, file)) for file in zip_ref.namelist()):
+                print(f"The files in this zip folder {filename} do not exist in the Daz library. Do you want to copy them? (yes/no)")
+                answer = input().lower()
 
-                # Check if the file already exists in the Daz library
-                if os.path.exists(daz_filepath):
-                    print(f"The file {file} already exists in the Daz library.")
-                else:
-                    print(f"The file {file} does not exist in the Daz library. Do you want to install it? (yes/no)")
-                    answer = input()
+                # If the user wants to install the files, extract them to the Daz library
+                if answer in ["yes", "y"]:
+                    zip_ref.extractall(daz_library_path)
+                    print(f"The files from {filename} have been installed to the Daz library.")
 
-                    # If the user wants to install the file, extract it to the Daz library
-                    if answer.lower() == "yes":
-                        zip_ref.extract(file, daz_library_path)
-                        print(f"The file {file} has been installed to the Daz library.")
-
-        # After installing the content of the zip file, move the zip file to the specified folder
-        shutil.move(filepath, os.path.join(zip_files_folder_path, filename))
-        print(f"The zip file {filename} has been moved to {zip_files_folder_path}.")
+                    # After installing the content of the zip file, move the zip file to the specified folder
+                    shutil.move(filepath, os.path.join(zip_files_folder_path, filename))
+                    print(f"The zip file {filename} has been moved to {zip_files_folder_path}.")
